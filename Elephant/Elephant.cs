0c83643a2d3388369bef803fc85dbdfbbd8302fa
izuru775 <izuru775@gmail.com>
2021-10-31 13:19:41 +1100

    Initial commit

---
+++ Elephant/Elephant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Elephant
+{
+    public class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"My name is {Name}.");
+            Console.WriteLine($"My years are {EarSize} inches tall.");
+        }
+    }
+}
---
+++ Elephant/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant { Name = "Lloyd", EarSize = 40 };
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            
+            while (true)
+            {
+                char pressed = Console.ReadKey(true).KeyChar;
+                Console.WriteLine($"You pressed {pressed}");
+                if (pressed == '1')
+                {
+                    lloyd.WhoAmI();
+                }
+                else if (pressed == '2')
+                {
+                    lucinda.WhoAmI();
+                }
+                else if (pressed == '3')
+                {
+                    Elephant holder;
+                    holder = lloyd;
+                    lloyd = lucinda;
+                    lucinda = holder;
+                    Console.WriteLine("Reference have been swapped");
+                }
+                else return;
+                Console.WriteLine();
+            }
+           
+            
+        }
+    }
+}
