e5c4754e61a13ac4d2e227892de2df35b0e312dc
izuru775 <izuru775@gmail.com>
2021-10-31 15:06:22 +1100

    HearMessage & SpeakTo methods added

--- Elephant/Elephant.cs
+++ Elephant/Elephant.cs
@@ -11,5 +11,14 @@ namespace Elephant
             Console.WriteLine($"My name is {Name}.");
             Console.WriteLine($"My years are {EarSize} inches tall.");
         }
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine(Name + " heard a message");
+            Console.WriteLine(whoSaidIt.Name + " said this: " + message);
+        }
+        public void SpeakTo(Elephant whoToTalkTo,string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
     }
 }
